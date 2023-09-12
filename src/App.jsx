a96e4cf6dc7d2a896417163bb9c966f5b9fdd100
Sergi-oh <sehernan96@gmail.com>
2023-09-12 01:35:09 -0700

    progression

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect} from 'react'
+import axios from 'axios';
+import { Link, useLocation, Routes, Route, useParams } from 'react-router-dom';
+import Home from './Home';
+import Users from './Users';
+import Posts from './Posts';
+import Post from './Post';
+
+const Home = ()=> {
+  return(
+    <h1>Home</h1>
+  );
+};
+
+const Users = ( { users })=> {
+  return (
+    <div>
+    <h1>Users</h1>
+    <ul>
+      {
+        users.map( user => {
+          return (
+            <li key={ user.id }> 
+              {user.name}
+            </li>
+          );
+        })
+      }
+    </ul>
+    </div>
+  );
+};
+
+const Posts = ({posts})=>{
+  return (
+    <h1>Posts</h1>
+  );
+};
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const location = useLocation();
+  const {pathname} = location;
+
+  useEffect(()=> {
+    const fetchUsers = async()=>{
+      const response = await axios.get('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users');
+      const data = response.data;
+      setUsers(data.data);
+    };
+    fetchUsers();
+  },[]);
+
+  useEffect(()=>{
+    const fetchPosts = async () =>{
+      const response = await axios.get('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/posts');
+      const data = response.data;
+      setPosts(data.data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      <nav>
+        <Link to='/' className={ pathname === '/' ? 'selected': ''}>Home</Link>        
+        <Link to='/users' className={ pathname === '/users' ? 'selected': ''}>Users ({users.length})</Link>
+        <Link to='/posts' className={ pathname === '/posts' ? 'selected': ''}>Posts</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/users' element={ <Users users={ users}/>} />
+        <Route path='/posts' element={ <Posts posts={ posts }/>} />
+        <Route path='/posts/:id' element={ <Post posts={ posts }/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
